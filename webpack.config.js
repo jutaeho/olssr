1f620d93bd7c56a65c9fe2ee788959e7d2aa73e2
jutaeho <dramavoy@daum.net>
2020-07-02 21:13:53 +0900

    webpack config js

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'js', 'App.js'),
+
+    mode: "development",
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader'],
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'OlSSr'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devServer: {
+        contentBase: './dist'
+    }
+}
